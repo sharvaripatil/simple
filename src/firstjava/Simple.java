73b0d977bbb5ff6aba0a9522ffeef2d6eff20623
Sharvari <Sharvari@Salman-PC.A4TECH.LOCAL>
2015-01-23 14:18:18 +0530

    this is the second change

--- src/firstjava/Simple.java
+++ src/firstjava/Simple.java
@@ -11,11 +11,14 @@ public void display()
 int c= a+b;
 System.out.println("addition is " +c);
 System.out.println("simple addition");
-}
+System.out.println("sharvari");
+System.out.println("this is the second change");
+}	
 	
 public static void main(String[] args)
 	{
-	Simple s=new Simple();
+	Simple s=new Simple(); 
+	//s=null;
 	s.display();
 	
 	}
